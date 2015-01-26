371d338489e2bb6d8a728009facf4bdfbf63622b
Fredrik Rambris <fredrik@rambris.com>
2015-01-26 15:44:20 +0100

    Added CountFileRows

--- src/com/rambris/Util.java
+++ src/com/rambris/Util.java
@@ -1,5 +1,9 @@
 package com.rambris;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.text.DateFormat;
@@ -534,6 +538,31 @@ public class Util
 			link = link.substring(0, link.length() - 1);
 		}
 		return result;
+	}
+	
+	/**
+	 * Count number of rows in file
+	 * @param path
+	 * @return
+	 * @throws IOException
+	 */
+	public static long CountFileRows(File path) throws IOException
+	{
+		BufferedReader in=null;
+		try
+		{
+			in=new BufferedReader(new InputStreamReader(GZip.OpenGZipFile(path)));
+			long rows=0;
+			while(in.readLine()!=null) rows++;
+			return rows;
+		}
+		finally
+		{
+			if(in!=null)
+			{
+				in.close();
+			}
+		}
 	}
 
 }
